b2151e27b22128007691718ce0096b2db9a4a4fd
Noe <nl@datalabmx.com>
2019-12-08 23:22:38 -0600

    con datos simplificados

--- js/map.js
+++ js/map.js
@@ -3,29 +3,17 @@ const pathGenerator = d3.geoPath()
 
 
 function addLayer() {
-  d3.json('./data/manhattan_geometries_sim2.geojson').then((data) => {
+  d3.json('./data/manhattan.json').then((data) => {
     let fitHeight;
     const field = 'parent_safegraph_place_id';
-
-    const nestedFeatures = d3.nest()
-      .key(d => d.properties[field])
-      .entries(data.features);
-    
-    const isolated = nestedFeatures
-      .filter(d => d.key === 'null' || d.values.length == 1)
-      .map(d => d.values)
-      .flat();
-    const grouped = nestedFeatures
-      .filter(d => d.key !== 'null' && d.values.length > 1); 
-    const groupedParentIds = grouped.map(d => d.key);
-
+    const collection = topojson.feature(data,data.objects.manhattan);
     const svg = d3.select('svg#map');
     const g = svg.append('g');
     const pathProjection = pathGenerator.projection();
     
-    pathGenerator.projection(pathProjection.fitWidth(1200,data));
+    pathGenerator.projection(pathProjection.fitWidth(1100,collection));
 
-    g.selectAll('path').data(data.features)
+    g.selectAll('path').data(collection.features)
       .enter().append('path')
         .attr('class','polygon')
         .attr('d',pathGenerator);
@@ -33,13 +21,14 @@ function addLayer() {
     fitHeight = g.node().getBBox().height;
     svg.style('height',Math.ceil(fitHeight));
 
+    g.selectAll('path')
+      .style('fill','#CCC')
+      .transition()
+        .delay(3000)
+        .duration(2000)
+        .style('fill',d => `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`);
 
-    // select "duplicates"
-    g.selectAll('path.polygon')
-      .filter(d => groupedParentIds.includes(d.properties[field]))
-      .style('fill','#CCC');
-
-    g.call(d3.zoom().on('zoom',() => zoom(g)));
+    //g.call(d3.zoom().on('zoom',() => zoom(g)));
     
   });
 };
@@ -47,4 +36,6 @@ function addLayer() {
 const zoom = (selection) => {
   console.log('d3.event.transform :', d3.event.transform);
   selection.attr('transform',d3.event.transform);
-}
+};
+
+const randomRgb = () => Math.floor(Math.random() * 255);
