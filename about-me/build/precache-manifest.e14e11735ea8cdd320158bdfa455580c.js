0b17f9f56a18e3f10b73959b7af9eec0fed81856
Noe <nl@datalabmx.com>
2020-01-29 19:45:01 -0600

    about me page

---
+++ about-me/build/precache-manifest.e14e11735ea8cdd320158bdfa455580c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae23ffd836d139d02e950230241adb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "330047f45f7d7f79d256",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6cfe6cc747bf8c8dab3b",
+    "url": "/static/js/2.adf47682.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.adf47682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "330047f45f7d7f79d256",
+    "url": "/static/js/main.2fd4617e.chunk.js"
+  },
+  {
+    "revision": "2ffad3dc2e0150f90caf",
+    "url": "/static/js/runtime-main.327afaab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
